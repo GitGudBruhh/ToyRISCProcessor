21be4fad2d0492b8c199e1728b414f40e30999d5
GitGudBruhh <gitgudbruhh@tuta.io>
2024-01-14 18:26:14 +0100

    deleted main.class instead of main.java

---
+++ Assignment_0/GitGud/Main.java
@@ -0,0 +1,19 @@
+// package DutyCycling;
+// import blah.rbrb;
+import DutyCycling.*;
+// import DutyCycling.Border;
+// import DutyCycling.Clock;
+// import DutyCycling.Infiltrator;
+// import DutyCycling.Sensor;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Clock clock;
+        Border border = new Border(20);
+        Infiltrator infiltrator = new Infiltrator(5,0);
+
+
+    }
+}
