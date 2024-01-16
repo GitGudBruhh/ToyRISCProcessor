bbd55dbee139497572b70b6d7116fc4d8753ce73
GitGudBruhh <gitgudbruhh@tuta.io>
2024-01-16 19:27:33 +0100

    Updated

--- Assignment_0/GitGud/main.py
+++ Assignment_0/GitGud/main.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-# for border_width in range(10, 100, 10):
-#     for sensor_prob_percent_val in range(5, 25, 5):
-#         sensor_prob = sensor_prob_percent_val/100
-#         print(border_width, sensor_prob)
-#         subprocess.call(['java', 'Main', str(border_width), str(sensor_prob), 'output.txt'])
+for border_width in range(10, 100, 10):
+    for sensor_prob_percent_val in range(5, 25, 5):
+        sensor_prob = sensor_prob_percent_val/100
+        print(border_width, sensor_prob)
+        subprocess.call(['javac', 'Main.java'])
+        subprocess.call(['java', 'Main', str(border_width), str(sensor_prob), 'output.txt'])
 
 width_list = []
 prob_list = []
