a1c1a762583e7a58aeb7a0ad305209e2480ebc54
GitGudBruhh <gitgudbruhh@tuta.io>
2024-02-11 13:47:18 +0100

    more functionality implemented, arithmetic instruction writing pending

--- Assignment_2/GitGud/CS311_assignment2/src/generic/Simulator.java
+++ Assignment_2/GitGud/CS311_assignment2/src/generic/Simulator.java
@@ -38,7 +38,6 @@ public class Simulator {
 		//2. write the firstCodeAddress to the file
 		//============================================================
 
-
 				//REFER TO THIS: 	https://stackoverflow.com/questions/2183240/java-integer-to-byte-array
 				//					https://docs.oracle.com/javase/6/docs/api/java/nio/ByteBuffer.html
 
@@ -49,54 +48,121 @@ public class Simulator {
 		//============================================================
 		//3. write the data to the file
 		//============================================================
-			int dataArrayListSize = ParsedProgram.data.size();
+				int dataArrayListSize = ParsedProgram.data.size();
 
-			for(int idx = 0; idx < dataArrayListSize; idx++)
-			{
-				Integer currentData;
-				currentData = ParsedProgram.data.get(idx); //Returns one line of data as Integer
+				for(int idx = 0; idx < dataArrayListSize; idx++) {
+					Integer currentData;
+					currentData = ParsedProgram.data.get(idx); //Returns one line of data as Integer
 
-				bytesToWrite = ByteBuffer.allocate(4).putInt(currentData.intValue()).array();
+					bytesToWrite = ByteBuffer.allocate(4).putInt(currentData.intValue()).array();
 					oStreamObjProgFile.write(bytesToWrite);
-			}
+				}
 
 
 		//============================================================
 		//4. assemble one instruction at a time, and write to the file
 		//============================================================
-			int codeArrayListSize = ParsedProgram.code.size();
+				int codeArrayListSize = ParsedProgram.code.size();
 
-			for(int idx = 0; idx < codeArrayListSize; idx++)
-			{
-				Instruction currentInstruction;
-				currentInstruction = ParsedProgram.code.get(idx); //Returns one line of data as Instruction
+				for(int idx = 0; idx < codeArrayListSize; idx++) {
+					Instruction currentInstruction;
+					currentInstruction = ParsedProgram.code.get(idx); //Returns one line of data as Instruction
 
-				OperationType currentOperationType = currentInstruction.getOperationType();
-				Operand currentSourceOpnd1 = currentInstruction.getSourceOperand1();
-				Operand currentSourceOpnd2 = currentInstruction.getSourceOperand2();
-				Operand currentDestOpnd = currentInstruction.getDestinationOperand();
+					Instruction.OperationType currentOperationType = currentInstruction.getOperationType();
+					Operand currentSourceOpnd1 = currentInstruction.getSourceOperand1();
+					Operand currentSourceOpnd2 = currentInstruction.getSourceOperand2();
+					Operand currentDestOpnd = currentInstruction.getDestinationOperand();
 
-				int currentInstructionAsInt = 0;
+					Operand.OperandType opndTypeRs1;
+					Operand.OperandType opndTypeRs2;
+					Operand.OperandType opndTypeRd;
 
-				//Convert operation to opcode(int) and shift left
-				currentInstructionAsInt += currentOperationType.ordinal() << 27;
+					opndTypeRs1 = (currentSourceOpnd1 == null) ? null : currentSourceOpnd1.getOperandType();
+					opndTypeRs2 = (currentSourceOpnd2 == null) ? null : currentSourceOpnd2.getOperandType();
+					opndTypeRd = (currentDestOpnd == null) ? null : currentDestOpnd.getOperandType();
 
-				//Checkout jmp, outlier
-				if(currentSourceOpnd1.getOperandType().ordinal() == 0 && currentSourceOpnd1.ge)
-				currentInstructionAsInt += currentSourceOpnd1 << 22;
-				/*
-				For any operand no. <= 21, all odd ones are immediate type, all even are register type.
-				*/
+					//For any arithmetic operation, imm values/label are kept in sourceOpnd2
+					//Opcodes: 0 -> 21 (Odd no.s)
+
+					//For loads and stores, imm values/label
+					//imm values are kept in sourceOpnd2
+					//Opcodes: 22, 23
+
+					//In jmp statements, either imm/label = 0, or rd = 0.
+					//The non zero value is kept in destOpnd
+					//Opcode: 24
+
+					//In conditional branches, sourceOpnd1 is compared to sourceOpnd2; immediate/label
+					//is in	destOpnd.
+					//Opcodes: 25 -> 28
+
+					//In end instruction, rd and imm are unused
+					//Opcode: 29
+
+					//So, imm values (as labels), can be in destOpnd, sourceOpnd2, but never sourceOpnd1
+
+					int currentInstructionAsInt = 0;
+
+					//Convert operation to opcode(int) and shift left
+					currentInstructionAsInt += (currentOperationType.ordinal() << 27);
+
+					//currentInstructionAsInt += currentSourceOpnd1.getValue() << 22;
+					//currentInstructionAsInt += currentSourceOpnd2.getValue() << 17;
+
+					//RI type (jmp & end)
+					if(currentSourceOpnd1 == null) {
+
+						//jmp
+						if(currentInstruction.getOperationType() == Instruction.OperationType.jmp) {
+
+							//Label
+							if(opndTypeRd == Operand.OperandType.Label) {
+								Integer addrOfLabel = ParsedProgram.symtab.get(currentDestOpnd.getLabelValue());
+								Integer valAtLabel = ParsedProgram.data.get(addrOfLabel.intValue());
+								currentInstructionAsInt += valAtLabel.intValue();
+							}
+
+							//Immediate
+							else if(opndTypeRd == Operand.OperandType.Immediate) {
+								currentInstructionAsInt += currentDestOpnd.getValue();
+							}
+
+							//Register
+							else
+								currentInstructionAsInt += (currentDestOpnd.getValue() << 22);
+
+						}
+
+						//end
+						// else if(currentInstruction.getOperationType() == Instruction.OperationType.end) {
+						// 	currentInstructionAsInt += 0;
+						// }
+					}
+
+					//R3 or R2I type
+					else {
+
+						Integer addrOfLabel;
+						Integer valAtLabel;
+
+						//Label to addrOfLabel to valAtLabel
+						if(opndTypeRs2 == Operand.OperandType.Label) {
+							addrOfLabel = ParsedProgram.symtab.get(currentSourceOpnd2.getLabelValue());
+							valAtLabel = ParsedProgram.data.get(addrOfLabel.intValue());
+						}
+
+					}
 
 
-				bytesToWrite = ByteBuffer.allocate(4).putInt(currentInstructionAsInt.intValue()).array();
+					bytesToWrite = ByteBuffer.allocate(4).putInt(currentInstructionAsInt).array();
 					oStreamObjProgFile.write(bytesToWrite);
-			}
+				}
 
 		//============================================================
 		//5. close the file
 		//============================================================
 
+				oStreamObjProgFile.close();
 			}
 			catch (IOException e) {
 				System.out.print("File Write Exception");
